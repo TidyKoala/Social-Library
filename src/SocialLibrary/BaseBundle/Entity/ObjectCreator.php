51114ec50edb4ed939ecfb7c29263640f630e01d
The Whole Life to Learn <thewholelifetolearn@gmail.com>
2012-11-30 17:05:33 +0100

    Created ObjectCreator entity

---
+++ src/SocialLibrary/BaseBundle/Entity/ObjectCreator.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace SocialLibrary\BaseBundle\Entity;
+
+use SocialLibrary\BaseBundle\Model\ObjectCreator as BaseObjectCreator;
+use Doctrine\ORM\Mapping as ORM;
+
+
+/**
+ * SocialLibrary\BaseBundle\Entity\ObjectCreator
+ * 
+ * @ORM\Entity
+ * @ORM\Table(name="object_creator")
+ */
+class ObjectCreator extends BaseObjectCreator
+{
+	/**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+	 * 
+	 * @var integer id
+	 */
+	protected $id;
+	
+	
+	/**
+     * {@inheritdoc}
+	 */
+	public function getId() {
+		return $this->id;
+	}
+}
