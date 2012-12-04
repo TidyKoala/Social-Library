1f12024cc4f0202e3b3c17f68c70385321697097
The Whole Life to Learn <thewholelifetolearn@gmail.com>
2012-12-04 13:10:30 +0100

    Passed config file from YML to XML

---
+++ src/SocialLibrary/BaseBundle/Admin/ObjectCreatorAdmin.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace SocialLibrary\BaseBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Validator\ErrorElement;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class ObjectCreatorAdmin extends Admin
+{
+	protected function configureFormFields(FormMapper $formMapper)
+	{
+		$formMapper
+		->add('firstname', null, array('required' => true))
+		->add('lastname', null, array('required' => false))
+		;
+	}
+
+	protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+	{
+		$datagridMapper
+		->add('firstname')
+		->add('lastname')
+		;
+	}
+
+	protected function configureListFields(ListMapper $listMapper)
+	{
+		$listMapper
+		->addIdentifier('nameSlug')
+		->add('firstname')
+		->add('lastname')
+		;
+	}
+
+	public function validate(ErrorElement $errorElement, $object)
+	{
+	}
+}
+?>
