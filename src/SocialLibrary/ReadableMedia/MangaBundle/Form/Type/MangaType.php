6a2e6088f09e88426eea2486a5370a4be8dd2c2a
The Whole Life to Learn <thewholelifetolearn@gmail.com>
2012-12-06 20:55:26 +0100

    Added details

--- src/SocialLibrary/ReadableMedia/MangaBundle/Form/Type/MangaType.php
+++ src/SocialLibrary/ReadableMedia/MangaBundle/Form/Type/MangaType.php
@@ -2,23 +2,69 @@
 
 namespace SocialLibrary\ReadableMedia\MangaBundle\Form\Type;
 
+use SocialLibrary\BaseBundle\Form\Type\ObjectCreatorType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Doctrine\ORM\EntityRepository;
 
 class MangaType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('volume')
-            ->add('serie')
-            ->add('creators')
-            ->add('illustrators')
-            ->add('language')
-            ->add('isbn10')
-            ->add('isbn13')
+            ->add('name', 'text', array(
+                    'required' => true,
+                    'label' => 'manga_label_name',
+                ))
+            ->add('volume', 'text', array(
+                    'required' => true,
+                    'label' => 'manga_label_volume',
+                ))
+            ->add('serie', 'entity', array(
+                    'required' => true,
+                    'label' => 'manga_label_serie',
+                    'class' => 'SocialLibrary\ReadableMedia\MangaBundle\Entity\Serie',
+                    'query_builder' => function(EntityRepository $er) {
+                        return $er->createQueryBuilder('s')
+                            ->orderBy('s.nameSlug', 'ASC');
+                    },
+                    'multiple' => false,
+                ))
+            ->add('creators', 'entity', array(
+                    'required' => true,
+                    'label' => 'manga_label_creators',
+                    'class' => 'SocialLibrary\BaseBundle\Entity\ObjectCreator',
+                    'query_builder' => function(EntityRepository $er) {
+                        return $er->createQueryBuilder('c')
+                            ->orderBy('c.nameSlug', 'ASC');
+                    },
+                    'multiple' => true,
+                    'expanded' => false,
+                ))
+            ->add('illustrators', 'entity', array(
+                    'required' => true,
+                    'label' => 'manga_label_illustrators',
+                    'class' => 'SocialLibrary\BaseBundle\Entity\ObjectCreator',
+                    'query_builder' => function(EntityRepository $er) {
+                        return $er->createQueryBuilder('i')
+                            ->orderBy('i.nameSlug', 'ASC');
+                    },
+                    'multiple' => true,
+                    'expanded' => false,
+                ))
+            ->add('language', 'language', array(
+                    'required' => false,
+                    'label' => 'manga_label_language',
+                ))
+            ->add('isbn10', 'text', array(
+                    'required' => false,
+                    'label' => 'manga_label_isbn10',
+                ))
+            ->add('isbn13', 'text', array(
+                    'required' => false,
+                    'label' => 'manga_label_isbn13',
+                ))
         ;
     }
 
