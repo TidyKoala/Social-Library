de86739c80e4d86addae4893b42383a58c27b373
The Whole Life to Learn <thewholelifetolearn@gmail.com>
2012-12-13 20:27:15 +0100

    Added possibility to add an author/illustrator

---
+++ src/SocialLibrary/BaseBundle/Controller/ObjectCreatorController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace SocialLibrary\BaseBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use SocialLibrary\BaseBundle\Entity\ObjectCreator;
+use SocialLibrary\BaseBundle\Form\Type\ObjectCreatorType;
+
+/**
+ * Manga controller.
+ *
+ * @Route("/object-creator/{_locale}", defaults={"_locale" = "en"})
+ */
+class ObjectCreatorController extends Controller
+{
+    /**
+     * Displays a form to create a new Manga entity.
+     *
+     * @Route("/ajax-new", name="object_creator_ajax_new")
+     * @Template()
+     */
+    public function newAjaxAction()
+    {
+        if (false === $this->get('security.context')->isGranted('ROLE_USER')) {
+            throw new AccessDeniedException();
+        }
+        
+        $entity = new ObjectCreator();
+        $form   = $this->createForm(new ObjectCreatorType(), $entity);
+
+        return array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        );
+    }
+
+    /**
+     * Creates a new Manga entity.
+     *
+     * @Route("/ajax-create", name="object_creator_ajax_create", defaults={ "_format"="xml|json"})
+     * @Template("SocialLibraryReadableMediaMangaBundle:Serie:newAjax.html.twig")
+     * @Method("POST")
+     */
+    public function createAjaxAction(Request $request)
+    {
+        if (false === $this->get('security.context')->isGranted('ROLE_USER')) {
+            throw new AccessDeniedException();
+        }
+        
+        $entity = new ObjectCreator();
+        $form   = $this->createForm(new ObjectCreatorType(), $entity);
+        $response = array('code' => 400);
+        $errors = array();
+        $form->bind($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+            $response['code'] = 200;
+        }
+        
+        // TODO: send the error message when necessary
+        
+        $response = array_merge($response, array('id' => $entity->getId(), 'name' => $entity->getFullname(), 'error' => $errors ) );
+        
+        return new Response(json_encode($response), 200, array('Content-Type' => 'application/json'));
+    }
+}
---
+++ src/SocialLibrary/BaseBundle/Form/Type/ObjectCreatorAjaxType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace SocialLibrary\BaseBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Doctrine\ORM\EntityRepository;
+
+class ObjectCreatorType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstname', 'text', array(
+                    'required' => true,
+                    'label' => 'object_creator_label_firstname',
+                    'error_bubbling' => true,
+                ))
+            ->add('lastname', 'text', array(
+                    'required' => false,
+                    'label' => 'object_creator_label_lastname',
+                    'error_bubbling' => true,
+                ))
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'SocialLibrary\BaseBundle\Entity\ObjectCreator'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'sociallibrary_basebundle_objectcreatortype';
+    }
+}
