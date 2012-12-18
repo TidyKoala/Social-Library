94819375c6e0f6783a1e89714d0bc535b9cabe54
The Whole Life to Learn <thewholelifetolearn@gmail.com>
2012-12-18 16:17:02 +0100

    Basic locale setup on index page according the browser preferences

--- src/SocialLibrary/BaseBundle/Controller/WelcomeController.php
+++ src/SocialLibrary/BaseBundle/Controller/WelcomeController.php
@@ -2,6 +2,8 @@
 
 namespace SocialLibrary\BaseBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Session\Session;
+
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
@@ -21,6 +23,11 @@ class WelcomeController extends Controller
      */
     public function indexAction()
     {
+        $languages = $this->getRequest()->getLanguages();
+        if( count($languages) > 0 ) {
+            $this->get('session')->set('_locale', $languages[0]);
+            $this->get('session')->save();
+        }
         return array();
     }
 }
