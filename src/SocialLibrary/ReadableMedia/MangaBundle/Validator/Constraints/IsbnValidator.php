d21fc5a67004c7d659adc9efc4f2b88b1458bcce
The Whole Life to Learn <thewholelifetolearn@gmail.com>
2013-01-14 11:25:46 +0100

    Corrected to PSR valid

--- src/SocialLibrary/ReadableMedia/MangaBundle/Validator/Constraints/IsbnValidator.php
+++ src/SocialLibrary/ReadableMedia/MangaBundle/Validator/Constraints/IsbnValidator.php
@@ -18,65 +18,61 @@ class IsbnValidator extends ConstraintValidator
      */
     public function validate($value, Constraint $constraint)
     {
-        $validation = null;
-        $checkIsbn10 = false;
-        $checkIsbn13 = false;
-        
         if (null === $value || '' === $value) {
             return;
         }
-        
+
         if (!is_scalar($value) && !(is_object($value) && method_exists($value, '__toString'))) {
             throw new UnexpectedTypeException($value, 'string');
         }
-        
-        $value = (string) $value;
-        $value = strtoupper($value);
 
-        if (!is_int($value)) {
+        if (!is_numeric($value)) {
             $value = str_replace('-', '', $value);
         }
         
-        if (10 == strlen($value)) {
+        $validation = 0;
+        $value = strtoupper($value);
+        $valueLength = strlen($value);
+
+        if (10 === $valueLength && null !== $constraint->isbn10) {
             for ($i = 0; $i < 10; $i++) {
-                if($value[$i] == 'X') {
+                if ($value[$i] == 'X') {
                     $validation += 10 * intval(10 - $i);
-                }
-                else {
+                } else {
                     $validation += intval($value[$i]) * intval(10 - $i);
                 }
             }
-    
-            if($validation % 11 == 0) {
-                $checkIsbn10 = true;
-            }
-        }
-        
-        if (13 == strlen($value)) {
-            for ($i = 0; $i < 13; $i+=2) {
-                    $validation += intval($value[$i]);
-            }
-            for ($i = 1; $i < 12; $i+=2) {
-                    $validation += intval($value[$i]) * 3;
-            }
-    
-            if($validation % 10 == 0) {
-                $checkIsbn13 = true;
-            }
-        }
 
-        if(null !== $constraint->isbn10 && null !== $constraint->isbn13 && $checkIsbn10 === false && $checkIsbn13 === false) {
-            $this->context->addViolation($constraint->bothIsbnMessage);
-            return;
-        }
+            if ($validation % 11 != 0) {
+                if (null !== $constraint->isbn13) {
+                    $this->context->addViolation($constraint->bothIsbnMessage);
+                } else {
+                    $this->context->addViolation($constraint->isbn10Message);
+                }
+            }
+        } elseif (13 === $valueLength && null !== $constraint->isbn13) {
+            for ($i = 0; $i < 13; $i += 2) {
+                $validation += intval($value[$i]);
+            }
+            for ($i = 1; $i < 12; $i += 2) {
+                $validation += intval($value[$i]) * 3;
+            }
 
-        if(null !== $constraint->isbn10 && null === $constraint->isbn13 && $checkIsbn10 === false) {
-            $this->context->addViolation($constraint->isbn10Message);
-            return;
-        }
-
-        if(null !== $constraint->isbn13 && null === $constraint->isbn10 && $checkIsbn13 === false) {
-            $this->context->addViolation($constraint->isbn13Message);
+            if ($validation % 10 != 0) {
+                if (null !== $constraint->isbn10) {
+                    $this->context->addViolation($constraint->bothIsbnMessage);
+                } else {
+                    $this->context->addViolation($constraint->isbn13Message);
+                }
+            }
+        } else {
+            if (null !== $constraint->isbn10 && null !== $constraint->isbn13) {
+                $this->context->addViolation($constraint->bothIsbnMessage);
+            } elseif (null !== $constraint->isbn10) {
+                $this->context->addViolation($constraint->isbn10Message);
+            } else {
+                $this->context->addViolation($constraint->isbn13Message);
+            }
         }
     }
 }
