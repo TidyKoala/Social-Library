83b0b3e7f2d120a5e6104c1a112fef347e20953f
Ralph KEMPS <thewholelifetolearn@gmail.com>
2013-02-24 10:32:33 +0100

    Deletes rests of GraphicNovel

--- src/SocialLibrary/ReadBundle/GraphicNovelBundle/SocialLibraryReadBundleGraphicNovelBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace SocialLibrary\ReadBundle\GraphicNovelBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class SocialLibraryReadBundleGraphicNovelBundle extends Bundle
-{
-}
